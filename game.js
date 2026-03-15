09b7822eaefd4949c49118f271e81977a1be8ef8
msimic1222 <markosimic1222@yahoo.com>
2026-03-15 16:58:58 +0000

    fixed bug where aliens were not moving on start

--- game.js
+++ game.js
@@ -327,7 +327,7 @@ function gameLoop(timeStamp){
 
             let sign = Math.sign(alienDirection);
 
-            alienDirection = sign * (2.5 + (15 - alive) / 4);
+            alienDirection = sign * (2.5 + (20 - alive) / 4);
 
             if(Math.abs(alienDirection) > 8)
                 alienDirection = sign * 8;
